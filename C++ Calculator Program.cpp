d7014f3e30c54ed1882772915297d553f210d657
john26999 <50142756+john26999@users.noreply.github.com>
2019-12-20 15:46:24 -0500

    Revert "Update C++ Calculator Program.cpp"

--- C++ Calculator Program.cpp
+++ C++ Calculator Program.cpp
@@ -1,62 +1,61 @@
-// Programmer: John Wilson
-// Program: C++ Calculator Program. This program will begin with simple arithmetic
-//          functions (add, sub, multi, div) and then will modify future functions
-//          to solve algebra, geometry , etc.
-//Date: 10 Dec 2019
-
-#include <iostream>     //library for cout
-#include <math.h>       //library for math functions
-#include <string>       //library for string functions
+#include <iostream>
+#include <math.h>
+#include <string>
 
 using namespace std;
 
-//this enum function is for the witch statement
 enum Arthmetic {ADD =1, SUB, MULT, DIV, EXIT};
 
-//This function includes instructions to how to use the program
-//Pre: Will have switch function
-//Post: None
 void WelcomeScreen ();
 
-//This function will have some fun ocular art in the shape of a calculator.
-//Pre: None.
-//Post: Will display a calculator.
 void WelcomeArt ();
 
-//This function will add an unlimited number of intergers together
-//Pre: Will declare int variables sum and numVal.
-//Post: Will add numbers and provide the solution.
-string Addition (int sum, int numVal);
+int Addition (int sum, int numVal);
 
-//This function will subtract an unlimited number of integers together
-//Pre: Will declare int variables sum and numVal.
-//Post: Will subtract numbers and provide the solution.
 int Subtraction (int sum);
 
-//This function will multiply an unlimited number of integers together
-//Pre: Will declare int variables sum and numVal.
-//Post: Will multiply numbers and provide the solution.
 int Multiplication (int sum);
 
-//This function will divide an unlimited number of integers together
-//Pre: Will declare int variables sum and numVal.
-//Post: Will divide numbers and provide the solution.
 int Division (int sum);
 
-//This function will display the solution to the math problems
-//Pre: none.
-//Post: Will display the solution to the math problems.
 void DisplayResult (int sum);
 
 main (){
 
-    int sum;                //local variable
-    int numVal;             //local variable
+    int sum;
+    int numVal;
 
-    //Function calls the welcome screen
     WelcomeScreen();
 
     return 0;
+}
+
+int Addition (int sum, int numVal){
+
+    int choice;
+    int count =0;
+    bool exitLoop = true;
+    char q;
+    sum=0;
+//    numVal =0;
+
+    cout << "Enter a positive or negative integer or 'q' to stop entering numbers: \n";
+//    cin >> numVal;
+    while (exitLoop) {
+            if (numVal == q) {
+
+                exitLoop = false;
+//  ++++++Test Thing++++++
+    cout << sum << endl;
+//  ++++++++++++++++++++++
+
+            } else {
+                cin >> numVal;
+                sum = sum + numVal;
+//                count++;
+            }
+
+    }
 }
 
 void WelcomeScreen(){
@@ -103,26 +102,6 @@ void WelcomeScreen(){
         }
 
     } while (choice != EXIT);
-}
-
-string Addition (int sum, int numVal){
-
-    int count = 0;       //Local variable that is initialized
-    char q;             //Local Variable
-    sum = 0;            //Local variable that is initialized
-
-    cout << "Enter a positive or negative integer and then press enter to continue "
-         << "adding new integers or press 'q' to stop entering numbers and for solution: \n";
-
-    while (! (numVal == q)) {
-        cin >> numVal;
-        sum = sum + numVal;
-//        count++;
-    }
-
-    //  ++++++Test Thing++++++
-        cout << sum << endl;
-    //  ++++++++++++++++++++++
 }
 
 int Subtraction (int sum) {

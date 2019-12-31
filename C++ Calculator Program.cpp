fbd2d9d84a4505eb2d06e92a7e1aad669f8898d6
john26999 <50142756+john26999@users.noreply.github.com>
2019-12-31 15:39:29 -0500

    Update C++ Calculator Program.cpp

--- C++ Calculator Program.cpp
+++ C++ Calculator Program.cpp
@@ -7,6 +7,7 @@
 #include <iostream>     //library for cout
 #include <math.h>       //library for math functions
 #include <string>       //library for string functions
+#include <limits>      //library for setting error checking
 
 using namespace std;
 
@@ -26,7 +27,7 @@ void WelcomeArt ();
 //This function will add an unlimited number of intergers together
 //Pre: Will declare int variables sum and numVal.
 //Post: Will add numbers and provide the solution.
-string Addition (int sum, int numVal);
+int Addition (int sum);
 
 //This function will subtract an unlimited number of integers together
 //Pre: Will declare int variables sum and numVal.
@@ -43,12 +44,17 @@ int Multiplication (int sum);
 //Post: Will divide numbers and provide the solution.
 int Division (int sum);
 
+//This function simply checks the user inputs integers and nothing else.
+//Pre:  none
+//Post: Will continue to loop the error until user inputs the info correctly
+string IntErrorChk ();
+
 //This function will display the solution to the math problems
 //Pre: none.
 //Post: Will display the solution to the math problems.
 void DisplayResult (int sum);
 
-main (){
+int main (){
 
     int sum;                //local variable
     int numVal;             //local variable
@@ -59,7 +65,7 @@ main (){
     return 0;
 }
 
-void WelcomeScreen(){
+void WelcomeScreen() {
 
     int sum;
     int numVal;
@@ -78,7 +84,8 @@ void WelcomeScreen(){
         switch (choice) {
 
             case ADD:
-                Addition(sum, numVal);
+                Addition(sum);
+                cout << endl;
                 break;
 
             case SUB:
@@ -105,24 +112,29 @@ void WelcomeScreen(){
     } while (choice != EXIT);
 }
 
-string Addition (int sum, int numVal){
+int Addition (int sum){
 
-    int count = 0;       //Local variable that is initialized
-    char q;             //Local Variable
+    int numVal;         //Local Variable
     sum = 0;            //Local variable that is initialized
+    int count;
 
     cout << "Enter a positive or negative integer and then press enter to continue "
-         << "adding new integers or press 'q' to stop entering numbers and for solution: \n";
+         << "adding new integers or press '-0' for solution and to return to the main menu: \n";
+    cin >> numVal;
 
-    while (! (numVal == q)) {
+    //Calls function for integer error checking
+    IntErrorChk ();
+
+     //do-while loop that continues to loop until the user wants the solution.
+     do {
+
         cin >> numVal;
-        sum = sum + numVal;
-//        count++;
-    }
+       sum = sum + numVal;
 
-    //  ++++++Test Thing++++++
-        cout << sum << endl;
-    //  ++++++++++++++++++++++
+     } while (numVal != -0);
+
+    // Function call to display the results of the integers
+    DisplayResult (sum);
 }
 
 int Subtraction (int sum) {
@@ -144,4 +156,35 @@ int Division (int sum){
 //  ++++++Test Thing++++++
     cout << "Div works! \n\n";
 //  ++++++++++++++++++++++
+}
+
+string IntErrorChk (){
+
+ int numVal;
+
+    while (1) {
+
+        if (cin.fail()) {  //This function returns true when an input failure occurs.
+
+            //cin.clear is used to clear the error state of the buffer so further processing of input can take place.
+            cin.clear ();
+
+            //cin.ignore is used to ignore the rest of the line after the first instance of error that has occurred.
+            cin.ignore (numeric_limits <streamsize>::max(), '\n');
+            cout << "You have entered incorrect info. Input integers only" <<endl;
+            cin  >> numVal;
+
+        } else if (!cin.fail()) {
+
+            break;
+        }
+    }
+}
+
+void DisplayResult (int sum) {
+
+    //  ++++++Test Thing++++++
+        cout << sum << endl;
+    //  ++++++++++++++++++++++
+
 }
